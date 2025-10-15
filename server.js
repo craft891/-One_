c3d366b8b586395ac4bd99e1161a957faf8e5692
craft891 <114077674+craft891@users.noreply.github.com>
2025-10-15 07:24:10 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,33 +5,27 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-// Increase the max payload size to allow for encrypted images
 const io = new Server(server, {
-  maxHttpBufferSize: 1e7 // 10 MB
+  maxHttpBufferSize: 1e8
 });
 
 const PORT = process.env.PORT || 3000;
 
-// Serve the static HTML file from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 let connectedUsers = 0;
 
-// Listen for new connections
 io.on('connection', (socket) => {
-  // A user has connected
   connectedUsers++;
   io.emit('user count', connectedUsers);
   console.log(`A user connected. Total users: ${connectedUsers}`);
 
-  // When an encrypted message is received, broadcast it to everyone else.
-  // The server doesn't know what's inside, it just passes it along.
+  
   socket.on('encrypted message', (encryptedData) => {
     socket.broadcast.emit('encrypted message', encryptedData);
   });
 
 
-  // Listen for a user disconnecting
   socket.on('disconnect', () => {
     connectedUsers--;
     io.emit('user count', connectedUsers);
